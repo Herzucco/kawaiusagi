6f9d5a8581314950e24e32e014473ada383d21b3
Herzucco <a.carta@student.isartdigital.com>
2014-12-07 23:28:32 +0100

    jumpers

--- TestTypeScript/src/decor/BasicObstacle.ts
+++ TestTypeScript/src/decor/BasicObstacle.ts
@@ -31,6 +31,7 @@ export class BasicObstacle extends g.GameObject {
 
     ratio : number;
     isJumping : boolean;
+    isJumper : boolean;
 
     constructor(x : number, y : number, z :number, ratio : number, color : string, sphereMesh : BABYLON.Mesh, scene : BABYLON.Scene) {
         super();
@@ -42,39 +43,45 @@ export class BasicObstacle extends g.GameObject {
         this.sphereMesh = sphereMesh;
         this.ratio = ratio;
         this.isJumping = false;
-        this.jumpHeight = 6;
+        this.jumpHeight = 2;
+        this.scene = scene;
 
         //creating, positioning and scaling the mesh
         var mesh : BABYLON.Mesh  = BABYLON.Mesh.CreateBox("box", this.ratio, scene);
         mesh.rotation.z = (Math.atan2(this.y - this.sphereMesh.position.y, this.x - this.sphereMesh.position.x));
         mesh.parent = this.sphereMesh;
         mesh.position = new BABYLON.Vector3(this.x,this.y, this.z);
-        mesh.scaling.x = 5;
+        mesh.scaling.y = 5;
 
         //get material for the mesh
         mesh.material = m.GetMaterial(color+"_Obstacle");
-        mesh.jumpAnimationVector = new BABYLON.Vector3(0, 3, 0);
+        mesh.jumpAnimationVector = new BABYLON.Vector3(0, 0.5, 0);
 
-        var keys = [];
-        keys.push({
-            frame: 0,
-            value: new BABYLON.Vector3(0, 3, 0)
-        },{
-            frame: 10,
-            value: new BABYLON.Vector3(0, this.jumpHeight-1, 0)
-        },{
-            frame: 15,
-            value: new BABYLON.Vector3(0, this.jumpHeight, 0)
-        },{
-            frame: 25,
-            value: new BABYLON.Vector3(0, 3, 0)
-        });
+        if(Math.random()*10 > 7.5){
+            var keys = [];
+            keys.push({
+                frame: 0,
+                value: new BABYLON.Vector3(0, 0.5, 0)
+            },{
+                frame: 25,
+                value: new BABYLON.Vector3(0, this.jumpHeight-1, 0)
+            },{
+                frame: 40,
+                value: new BABYLON.Vector3(0, this.jumpHeight, 0)
+            },{
+                frame: 60,
+                value: new BABYLON.Vector3(0, 0.5, 0)
+            });
 
-        var jumpAnimation = new BABYLON.Animation("anim", "jumpAnimationVector", 60, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
-        jumpAnimation.setKeys(keys);
-        mesh.animations.push(jumpAnimation);
+            var jumpAnimation = new BABYLON.Animation("anim", "jumpAnimationVector", 60, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+            jumpAnimation.setKeys(keys);
+            mesh.animations.push(jumpAnimation);
+
+            this.isJumper = true;
+        }
 
         this.mesh = mesh;
+
     }
 
     endJump(){
@@ -83,13 +90,18 @@ export class BasicObstacle extends g.GameObject {
 
     update(deltaTime : number){
         super.update(deltaTime);
-        //this.mesh.setPositionWithLocalVector(this.mesh.jumpAnimationVector);
+
+        this.mesh.setPositionWithLocalVector(this.mesh.jumpAnimationVector);
+        this.jump();
     }
 
     jump(){
-        this.scene.beginAnimation(this.mesh, 0, 25, false, 1,
-                                    this.endJump.bind(this));
+        if(!this.isJumping && this.isJumper){
+            this.scene.beginAnimation(this.mesh, 0, 60, false, 1,
+                this.endJump.bind(this));
 
-        this.isJumping = true;
+            this.isJumping = true;
+        }
+
     }
 }
